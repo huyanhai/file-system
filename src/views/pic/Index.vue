161a6bd9a01a6ea7da909db648e4b8de2bd90184
huyanhai <810839700@qq.com>
2022-10-18 16:02:18 +0800

    style: ui美化

--- src/views/pic/Index.vue
+++ src/views/pic/Index.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="tw-h-full" id="dropDom" ref="dropDom" @drop="drop" @dragover="dragover">
-    <div v-for="item in fileArr" class="tw-flex tw-items-center">
-      <div class="tw-w-10 tw-h-10 tw-flex tw-items-center tw-justify-center">
-        <img class="tw-flex tw-max-w-full tw-max-h-full" :src="item.base64" alt="" />
+  <div class="tw-h-full tw-overflow-y-scroll" id="dropDom" ref="dropDom" @drop="drop" @dragover="dragover">
+    <div v-for="item in fileArr" class="tw-flex tw-items-center tw-bg-white tw-p-2 tw-m-2 tw-rounded-md tw-justify-between">
+      <div class="tw-flex tw-items-center tw-w-4/12">
+        <div class="tw-w-10 tw-h-10 tw-flex tw-items-center tw-justify-center">
+          <img class="tw-flex tw-max-w-full tw-max-h-full" :src="item.base64" alt="" />
+        </div>
+        <p class="tw-text-xs tw-m-0 tw-w-1/2 tw-mx-2 tw-overflow-hidden tw-whitespace-nowrap tw-text-ellipsis">{{ item.name }}</p>
       </div>
-      <p class="tw-text-xs tw-m-0 tw-w-1/4 tw-mx-2 tw-overflow-hidden tw-whitespace-nowrap tw-text-ellipsis">{{ item.name }}</p>
-      <p class="tw-m-0 tw-text-xs tw-w-10 tw-text-right">{{ item.size / 1000 > 1 ? `${Math.ceil(item.size / 1000)}kb` : `${item.size % 1000}b` }}</p>
-      <div class="tw-w-1/3 tw-mx-2">
-        <a-progress :percent="100" :status="item.byteLength ? 'success' : 'active'" :show-info="false" />
+      <div class="tw-flex tw-items-center tw-w-5/12">
+        <p class="tw-m-0 tw-text-xs tw-w-10 tw-text-right">{{ item.size / 1000 > 1 ? `${Math.ceil(item.size / 1000)}kb` : `${item.size % 1000}b` }}</p>
+        <div class="tw-w-1/3 tw-mx-2">
+          <a-progress :percent="100" :status="item.byteLength ? 'success' : 'active'" :show-info="false" />
+        </div>
+        <p class="tw-m-0 tw-text-xs tw-w-5" v-if="item.byteLength">{{ item.byteLength / 1000 > 1 ? `${Math.ceil(item.byteLength / 1000)}kb` : `${item.byteLength % 1000}b` }}</p>
+        <p class="tw-m-0 tw-text-xs tw-ml-4 tw-w-10 tw-text-right" v-if="item.byteLength">{{ Math.ceil((1 - item.byteLength / item.size) * 100) }}%</p>
       </div>
-      <p class="tw-m-0 tw-text-xs tw-w-5" v-if="item.byteLength">{{ item.byteLength / 1000 > 1 ? `${Math.ceil(item.byteLength / 1000)}kb` : `${item.byteLength % 1000}b` }}</p>
-      <p class="tw-m-0 tw-text-xs tw-ml-4 tw-w-10 tw-text-right" v-if="item.byteLength">{{ Math.ceil((1 - item.byteLength / item.size) * 100) }}%</p>
-      <a @click="download(item)">下载</a>
+      <a class="tw-w-1/12 tw-text-right tw-text-xs" @click="download(item)">下载</a>
     </div>
   </div>
 </template>
